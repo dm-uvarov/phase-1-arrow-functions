1f030f34efa73ea3465b0f655d8856b138515826
Jimmy <dm.a.uvarov@gmail.com>
2022-01-11 11:56:18 -0500

    commit tree 4aeea7a1ab986fc9ffea49466eb71fb3a7f8f31c

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide  = function(){
+    return 2000/100;
+}// end of divide
+
+const square = parameter1 => parameter1*parameter1;  
+
+const add = (parameter3, parameter2) => parameter3 + parameter2; 
